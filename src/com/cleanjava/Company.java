8410127a6651d63b351eddf5aade223f1394caea
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-31 17:31:55 +0300

    add java collection member variables with their getter and setter methods

--- src/com/cleanjava/Company.java
+++ src/com/cleanjava/Company.java
@@ -1,19 +1,59 @@
 package com.cleanjava;
 
-public class Company {
-	private String name, location;
-	
-	
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+public class Company {
+	
+	private String name, location;
+	private Address address;
+	private List<Employee> employees;
+	private Set<String> departments;
+	private Map<String, Employee> employeeDepartments;
+	
+	
+	
+	public Company( Address add) {
+		this.address = add;
+	}
+	
 	public Company() {
 	}
-	
-	
-	public Company(String name, String location) {
-		this.name = name;
-		this.location = location;
+
+
+	public List<Employee> getEmployees() {
+		return employees;
 	}
 
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	public Set<String> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(Set<String> departments) {
+		this.departments = departments;
+	}
+
+	public Map<String, Employee> getEmployeeDepartments() {
+		return employeeDepartments;
+	}
+
+	public void setEmployeeDepartments(Map<String, Employee> employeeDepartments) {
+		this.employeeDepartments = employeeDepartments;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
 
 	public String getName() {
