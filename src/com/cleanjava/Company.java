35c6fa346201d00a8857a1ab55cb7a0fe0430058
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-31 10:27:10 +0300

    add an init callback method to be executed when an instance of the bean is created

--- src/com/cleanjava/Company.java
+++ src/com/cleanjava/Company.java
@@ -1,5 +1,40 @@
 package com.cleanjava;
 
 public class Company {
+	private String name, location;
+	
+	
+
+	public Company() {
+	}
+	
+	
+	public Company(String name, String location) {
+		this.name = name;
+		this.location = location;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public void init() {
+		System.out.println("A company instance has been created, Please do something");
+	}
+	
 
 }
