52c3dd9f9da3e374a2ea3abb3ac7859ac6f34e81
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-31 09:49:59 +0300

    create a BeanFactory implemenation to read configuration from the beans xml file

--- src/com/cleanjava/Example.java
+++ src/com/cleanjava/Example.java
@@ -1,16 +1,26 @@
 package com.cleanjava;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ApplicationContext context  = new ClassPathXmlApplicationContext("Beans.xml");
-		Employee e = (Employee) context.getBean("employee");
-		System.out.println(e.getSalary());
+		// Spring IoC containers
+		// 1. BeanFactory - provides basic support for Di and used for small applications
+		// 2. ApplicationConext
+		
+		//BeanFactory implementation
+		//XmlBeanFactory class is an implemantaion that reads metadata from an xml configuration file and develops an application.
+		
+		BeanFactory factory  = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+		Employee emp = (Employee) factory.getBean("employee");
+		System.out.println(emp.getAge());
 		
 		
 	}
